5d1f459e6b3c6bf4cd714b8d2fdcbee240c4c12f
tornikejincharadze <34689319+tornikejincharadze@users.noreply.github.com>
2018-01-07 19:08:42 +0400

    Summary

--- js/menu-button.js
+++ js/menu-button.js
@@ -23,47 +23,50 @@ $(document).ready(function(){
     });
 });
 $(document).ready(function (){
-        $("#servicesB").click(function (){
-            $('html, body').animate({
-                scrollTop: $("#services").offset().top
-            }, 500);
-            $(".owl-prev").show();
-            $(".owl-next").show();
-            $(".item ul li.t-li , .item ul li.h-li").show();
-            $(".item img").css("opacity","1");
-            $(".logo .cls-1").css("fill","#FFFFFF");
-            $(".nav-open").show();
-            $(".nav-exit").hide();
-            $(".navigation-bar").hide();
+    $("#servicesB").click(function (){
+        $('html, body').animate({
+            scrollTop: $("#services").offset().top
+        }, 500);
+        $(".owl-prev").show();
+        $(".owl-next").show();
+        $(".item ul li.t-li , .item ul li.h-li").show();
+        $(".item img").css("opacity","1");
+        $(".logo .cls-1").css("fill","#FFFFFF");
+        $(".nav-open").show();
+        $(".nav-exit").hide();
+        $(".navigation-bar").hide();
+        $("#title").html("Services | სერვისები");
     });
 });
 $(document).ready(function (){
-        $("#homeB").click(function (){
-            $('html, body').animate({
-                scrollTop: $("#home").offset().top
-            }, 500);
-            $(".owl-prev").show();
-            $(".owl-next").show();
-            $(".item ul li.t-li , .item ul li.h-li").show();
-            $(".item img").css("opacity","1");
-            $(".logo .cls-1").css("fill","#FFFFFF");
-            $(".nav-open").show();
-            $(".nav-exit").hide();
-            $(".navigation-bar").hide();
+    $("#homeB").click(function (){
+        $('html, body').animate({
+             scrollTop: $("#home").offset().top
+        }, 500);
+        $(".owl-prev").show();
+        $(".owl-next").show();
+        $(".item ul li.t-li , .item ul li.h-li").show();
+        $(".item img").css("opacity","1");
+        $(".logo .cls-1").css("fill","#FFFFFF");
+        $(".nav-open").show();
+        $(".nav-exit").hide();
+        $(".navigation-bar").hide();
+        $("#title").html("Home | მთავარი");
     });
 });
 $(document).ready(function (){
-        $("#contactB").click(function (){
-            $('html, body').animate({
-                scrollTop: $("#contact").offset().top
-            }, 500);
-            $(".owl-prev").show();
-            $(".owl-next").show();
-            $(".item ul li.t-li , .item ul li.h-li").show();
-            $(".item img").css("opacity","1");
-            $(".logo .cls-1").css("fill","#FFFFFF");
-            $(".nav-open").show();
-            $(".nav-exit").hide();
-            $(".navigation-bar").hide();
+    $("#contactB").click(function (){
+        $('html, body').animate({
+            scrollTop: $("#contact").offset().top
+        }, 500);
+        $(".owl-prev").show();
+        $(".owl-next").show();
+        $(".item ul li.t-li , .item ul li.h-li").show();
+        $(".item img").css("opacity","1");
+        $(".logo .cls-1").css("fill","#FFFFFF");
+        $(".nav-open").show();
+        $(".nav-exit").hide();
+        $(".navigation-bar").hide();
+        $("#title").html("Contact | კონტაქტი");
     });
 });
